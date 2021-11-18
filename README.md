c8da322234e942c9465c58360c1d81af1d31fb0c
Enrique <eferraris@fie.undef.edu.ar>
2021-11-18 17:43:21 -0300

    MOD - defaults.

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ This Action helps you to send a message to a queue on a RabbitMQ running Server.
 | RABBIT_PASSWORD | guest | RabbitMQ Password  |
 | RABBIT_HOST | localhost | RabbitMQ Host |
 | RABBIT_PORT | 5672 | RabbitMQ Port |
-| RABBIT_USE_SSL | false | If the RabbitMQ uses SSL |
+| RABBIT_USE_SSL | true | If the RabbitMQ uses SSL |
 | RABBIT_QUEUE_NAME | rabbit-sender | Name of the queue the message must arrive |
 | MESSAGE | Hello World! | Message you want to send |
 
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -22,11 +22,14 @@ fun main() {
 private fun connectionFactory(): ConnectionFactory {
 
     val factory = ConnectionFactory()
+    val useSSL = System.getenv("INPUT_RABBIT_USE_SSL")
+        ?.let { it.toBoolean() }
+        ?: true
 
     factory.host = System.getenv("INPUT_RABBIT_HOST")?: "localhost"
     factory.username = System.getenv("INPUT_RABBIT_USERNAME")?: "guest"
     factory.password = System.getenv("INPUT_RABBIT_PASSWORD")?: "guest"
-    System.getenv("INPUT_RABBIT_USE_SSL")?.let { if ( it.toBoolean() ) factory.useSslProtocol() }
+    if ( useSSL ) factory.useSslProtocol()
     factory.port = System.getenv("INPUT_RABBIT_PORT")?.toInt()?: 5672
 
     return factory
