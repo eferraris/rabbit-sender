47e61fca15357d56ec24ec44e43939458e7326fa
Quique Ferraris <enrique.ferraris@zoomagri.com>
2023-06-09 16:20:49 -0300

    ADD - default fix.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,12 +3,12 @@ import com.rabbitmq.client.ConnectionFactory
 fun main() {
     val connection = connectionFactory().newConnection()
     val channel = connection.createChannel()
-    val message = System.getenv("INPUT_MESSAGE")
+    val message = System.getenv("INPUT_MESSAGE").takeIf { it.isNotEmpty() }
         ?.toByteArray()
         ?:"Hello World!".toByteArray()
-    val queueName = System.getenv("INPUT_RABBIT_QUEUE_NAME")?: "rabbit-sender"
-    val exchangeName = System.getenv("INPUT_RABBIT_EXCHANGE_NAME")?: ""
-    val durable = System.getenv("INPUT_DURABLE")?.toBoolean()?: true
+    val queueName = System.getenv("INPUT_RABBIT_QUEUE_NAME").takeIf { it.isNotEmpty() }?: "rabbit-sender"
+    val exchangeName = System.getenv("INPUT_RABBIT_EXCHANGE_NAME").takeIf { it.isNotEmpty() }?: ""
+    val durable = System.getenv("INPUT_DURABLE").takeIf { it.isNotEmpty() }?.toBoolean()?: true
 
     try {
         if ( exchangeName.isNotEmpty() ) {
@@ -31,16 +31,15 @@ private fun connectionFactory(): ConnectionFactory {
     val factory = ConnectionFactory()
     val useSSL = System
         .getenv("INPUT_RABBIT_USE_SSL")
+        .takeIf { it.isNotEmpty() }
         ?.toBoolean()
         ?: true
 
     factory.host = System.getenv("INPUT_RABBIT_HOST")?: "localhost"
-    factory.username = System.getenv("INPUT_RABBIT_USERNAME")?: "guest"
-    factory.password = System.getenv("INPUT_RABBIT_PASSWORD")?: "guest"
+    factory.username = System.getenv("INPUT_RABBIT_USERNAME").takeIf { it.isNotEmpty() }?: "guest"
+    factory.password = System.getenv("INPUT_RABBIT_PASSWORD").takeIf { it.isNotEmpty() }?: "guest"
     if ( useSSL ) factory.useSslProtocol()
-    factory.port = System.getenv("INPUT_RABBIT_PORT")?.toInt() ?: 5672
-
-    System.getenv().forEach { (k, v) -> println("$k=$v") }
+    factory.port = System.getenv("INPUT_RABBIT_PORT").takeIf { it.isNotEmpty() }?.toInt() ?: 5672
 
     return factory
 
