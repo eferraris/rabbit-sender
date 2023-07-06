87563e50c40b7d91ca502120702e549d9633d62c
Quique Ferraris <enrique.ferraris@zoomagri.com>
2023-07-06 12:45:50 -0300

    ADD - getEnvOrNull method.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,12 +3,12 @@ import com.rabbitmq.client.ConnectionFactory
 fun main() {
     val connection = connectionFactory().newConnection()
     val channel = connection.createChannel()
-    val message = System.getenv("INPUT_MESSAGE").takeIf { it.isNotEmpty() }
+    val message = getEnvOrNull("INPUT_MESSAGE")
         ?.toByteArray()
         ?:"Hello World!".toByteArray()
-    val queueName = System.getenv("INPUT_RABBIT_QUEUE_NAME").takeIf { it.isNotEmpty() }?: "rabbit-sender"
-    val exchangeName = System.getenv("INPUT_RABBIT_EXCHANGE_NAME").takeIf { it.isNotEmpty() }?: ""
-    val durable = System.getenv("INPUT_DURABLE").takeIf { it.isNotEmpty() }?.toBoolean()?: true
+    val queueName = getEnvOrNull("INPUT_RABBIT_QUEUE_NAME")?: "rabbit-sender"
+    val exchangeName = getEnvOrNull("INPUT_RABBIT_EXCHANGE_NAME")?: ""
+    val durable = getEnvOrNull("INPUT_DURABLE")?.toBoolean()?: true
 
     try {
         if ( exchangeName.isNotEmpty() ) {
@@ -25,22 +25,22 @@ fun main() {
     }
 
 }
-
 private fun connectionFactory(): ConnectionFactory {
 
     val factory = ConnectionFactory()
-    val useSSL = System
-        .getenv("INPUT_RABBIT_USE_SSL")
-        .takeIf { it.isNotEmpty() }
+    val useSSL = getEnvOrNull("INPUT_RABBIT_USE_SSL")
         ?.toBoolean()
         ?: true
 
-    factory.host = System.getenv("INPUT_RABBIT_HOST")?: "localhost"
-    factory.username = System.getenv("INPUT_RABBIT_USERNAME").takeIf { it.isNotEmpty() }?: "guest"
-    factory.password = System.getenv("INPUT_RABBIT_PASSWORD").takeIf { it.isNotEmpty() }?: "guest"
+    factory.host = getEnvOrNull("INPUT_RABBIT_HOST")?: "localhost"
+    factory.username = getEnvOrNull("INPUT_RABBIT_USERNAME")?: "guest"
+    factory.password = getEnvOrNull("INPUT_RABBIT_PASSWORD")?: "guest"
     if ( useSSL ) factory.useSslProtocol()
-    factory.port = System.getenv("INPUT_RABBIT_PORT").takeIf { it.isNotEmpty() }?.toInt() ?: 5672
+    factory.port = getEnvOrNull("INPUT_RABBIT_PORT")?.toInt() ?: 5672
 
     return factory
 
 }
+private fun getEnvOrNull(name: String): String? = System
+    .getenv( name )
+    ?.let { it.takeIf { it.isNotEmpty() } }
